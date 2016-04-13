ca8b19fd4e95f87b01a4c949b8a350c3f6334b8a
Talamalopilis <tal.melamed.69@gmail.com>
2016-04-12 19:02:19 -0700

    Parsing count and drawing it to display now works! Had to remove unneeded struct "digit" and scale down some ints to char because running out of flash memory.

--- countball/NumbersLibrary.h
+++ countball/NumbersLibrary.h
@@ -6,26 +6,28 @@
 #define PROGMEM
 #endif
 
-struct digit {
-	const unsigned char *bitmap;	//[30] 3*10
-	//digit display, 24x20 pixels, 8 pixels per column, width compressed to 10
-};
-
 struct numbers {
-	struct digit zero;
-	struct digit one;
-	struct digit two;
-	struct digit three;
-	struct digit four;
-	struct digit five;
-	struct digit six;
-	struct digit seven;
-	struct digit eight;
-	struct digit nine;
+	//digit display, 24x20 pixels, 8 pixels per column, width compressed to 10
+	const unsigned char *zero; //[30] 3*10
+	const unsigned char *one;
+	const unsigned char *two;
+	const unsigned char *three;
+	const unsigned char *four;
+	const unsigned char *five;
+	const unsigned char *six;
+	const unsigned char *seven;
+	const unsigned char *eight;
+	const unsigned char *nine;
 };
 
 const unsigned char zeroM[30] PROGMEM = {
 	0x00, 0xf0, 0xfc, 0x3c, 0x3c, 0x3c, 0x3c, 0xfc, 0xf0, 0x00,
 	0x00, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0x00,
 	0x00, 0x0f, 0x3f, 0x3c, 0x3c, 0x3c, 0x3c, 0x3f, 0x0f, 0x00
+};
+
+const unsigned char oneM[30] PROGMEM = {
+	0x00, 0x00, 0x3c, 0x3c, 0xfc, 0xfc, 0x3c, 0x3c, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x03, 0x0f, 0x3f, 0x3f, 0x00, 0x00, 0x00, 0x00
 };
--- countball/countball.c
+++ countball/countball.c
@@ -1,6 +1,5 @@
 #ifdef _MSC_VER			//IntelliSense override
 #define __AVR_ATtiny13__	
-#define pgm_read_byte
 #endif
 
 #define A0 PINB1
@@ -15,12 +14,16 @@
 #include <avr/portpins.h>
 #include "NumbersLibrary.h"
 
+static const unsigned char MAXDIGITS =	6;
+static unsigned char STARTCOLUMN =		104;
+static const char DIGITSIZE = 20;
+
 void init_LCD();
 void data_out(unsigned char);
 void comm_out(unsigned char);
 void paint_frame();
 void paint_count(unsigned int, struct numbers*);
-void paint_digit(struct digit*, unsigned char);
+void paint_digit(const unsigned char*, unsigned char);
 void init_numbers(struct numbers*);
 
 int main(void)
@@ -40,35 +43,22 @@ int main(void)
 
 	struct numbers numbers;
 	init_numbers(&numbers);
+	paint_count(10, &numbers);
+	//_delay_ms(15000);
+	//paint_count(1111, &numbers);
 
 	while (1)
 	{
-		paint_digit(&(numbers.zero), 4);
-		paint_digit(&(numbers.zero), 24);
-		paint_digit(&(numbers.zero), 44);
-		/*count++;
-		if (count % 10 == 0)
-			comm_out(0xB0);
-		_delay_ms(1500);*/
-		/*PORTB = _BV(PINB3);
-		_delay_ms(1500);
-		PORTB = 0;
-		_delay_ms(1500);
-		count++;
-		if (count > 10) {
-			set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-			sleep_enable();
-			sleep_cpu();
-		}*/
-
-
+		/*set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+		sleep_enable();
+		sleep_cpu();*/
 	}
 	return 0;
 }
 
 void data_out(unsigned char i) //Data Output Serial Interface
 {
-	unsigned int n;
+	unsigned char n;
 	unsigned char t;
 	PORTB |= _BV(A0); // A0 = 1
 	for (n = 0; n<8; n++) {
@@ -86,7 +76,7 @@ void data_out(unsigned char i) //Data Output Serial Interface
 }
 void comm_out(unsigned char i) //Command Output Serial Interface
 {
-	unsigned int n;
+	unsigned char n;
 	unsigned char t;
 	PORTB &= ~_BV(A0); // A0 = 0
 	for (n = 0; n<8; n++) {
@@ -109,28 +99,78 @@ void paint_frame()
 
 void paint_count(unsigned int count, struct numbers *numbers)
 {
+	unsigned char column = STARTCOLUMN; // start at last least significant digit
+	int digitnum;
+	const unsigned char *digitdraw;
+	char i;
+	int a = 10;
+	int b = 1;
+
+	for (i = 0; i < MAXDIGITS && count > 0; i++, b=a, a*=10, column-=DIGITSIZE) {
+		// paint until max digits or until out of inputed digits.
+		// increment digit count, order up dividor and modulus, shift column back one digit
+		digitnum = count % a;
+		count -= digitnum;
+		digitnum = digitnum / b;
+
+		switch (digitnum)
+		{
+		case 0:
+			digitdraw = numbers->zero;
+			break;
+		case 1:
+			digitdraw = numbers->one;
+			break;
+		case 2:
+			digitdraw = numbers->two;
+			break;
+		case 3:
+			digitdraw = numbers->three;
+			break;
+		case 4:
+			digitdraw = numbers->four;
+			break;
+		case 5:
+			digitdraw = numbers->five;
+			break;
+		case 6:
+			digitdraw = numbers->six;
+			break;
+		case 7:
+			digitdraw = numbers->seven;
+			break;
+		case 8:
+			digitdraw = numbers->eight;
+			break;
+		case 9:
+			digitdraw = numbers->nine;
+			break;
+		default:
+			digitdraw = numbers->zero;
+			break;
+		}
+		paint_digit(digitdraw, column);
+	}
 }
 
-void paint_digit(struct digit *digit, unsigned char column)
+void paint_digit(const unsigned char *digit, unsigned char column)
 {
-	int i, j;
+	char i, j;
 	for (i = 0; i < 3; i++) {
 		comm_out(0xB0 + i+1); //start at row 1
 		comm_out(0x10 + (column >> 4)); //column (upper bits)
 		comm_out(0x00 + (column & 0x0f)); //column (lower bits)
 		for (j = 0; j < 10; j++) {
-			data_out(pgm_read_byte(digit->bitmap + (i * 10 + j)));
-			data_out(pgm_read_byte(digit->bitmap + (i * 10 + j)));
-			_delay_ms(1500);
+			data_out(pgm_read_byte(digit + (i * 10 + j)));
+			data_out(pgm_read_byte(digit + (i * 10 + j)));
 		}
 	}
 }
 
 void init_numbers(struct numbers *n)
 {
-	//data_out(0xFF);
-
-	n->zero.bitmap = zeroM;
+	n->zero = zeroM;
+	n->one = oneM;
 }
 
 void init_LCD()
